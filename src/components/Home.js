e0a62e6f0113626730bfb493748a4ffa620ad120
Anabel <anabel.pmoraes@gmail.com>
2024-06-19 02:23:30 -0300

    fifth commit.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,24 +4,24 @@ const Home = () => {
   return (
     <div className="home">
       <div className="introduction">
-        <h1>Hello, I am Anabel</h1>
+        <h1 className="typing-animation">Hello, I am Anabel</h1>
         <p className="intro-text"> Backend Developer. </p>
         <p className="intro-text"> If you would like to know more about me feel free to browse!</p>
+        <a href="../misc/resume.pdf" className="resume-button" target="_blank" rel="noopener noreferrer">Resume</a>
     {/* Add other sections like About, Projects, Contact here */}
       </div>
-      <div className="skills">
-        <h2> Skills </h2>
-        <ul className="tree">
-          <li>CSS</li>
-          <li>HTML</li>
-          <li>Golang</li>
-          <li>Python</li>
-          <li>C</li>
-          <li>Javascript</li>
-          <li>SQL</li>
-          <li>Docker</li>
-          <li>Git</li>
-        </ul>
+      <div className="Skills">
+        <h2>Skills</h2>
+        <div className="skills-grid">
+          <div className="skill-item">CSS</div>
+          <div className="skill-item">HTML</div>
+          <div className="skill-item">Golang</div>
+          <div className="skill-item">Python</div>
+          <div className="skill-item">C</div>
+          <div className="skill-item">SQL</div>
+          <div className="skill-item">Docker</div>
+          <div className="skill-item">Git</div>
+        </div>
       </div>
     </div>
   );
