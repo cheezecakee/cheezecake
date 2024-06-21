02634ee76acde6fe72d72a11094da3de8d52e130
Anabel <anabel.pmoraes@gmail.com>
2024-06-21 17:48:20 -0300

    update.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 const Projects = () => {
   const [repos, setRepos] = useState([]);
   const username = 'cheezecakee'; 
-  const token = process.env.REACT_APP_GH;
+  const token = process.env.REACT_APP_GITHUB_TOKEN;
 
   useEffect(() => {
     const fetchRepos = async () => {
