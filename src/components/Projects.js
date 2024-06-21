f54fb8d7eb3b3a577664abeac2aeb5f3abb84b22
Anabel <anabel.pmoraes@gmail.com>
2024-06-21 10:42:11 -0300

    eleventh commit.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,29 +2,35 @@ import React, { useState, useEffect } from 'react';
 
 const Projects = () => {
   const [repos, setRepos] = useState([]);
-  const username = 'cheezecakee'; // Replace with your GitHub username
-  const token = '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'; // Replace with your GitHub personal access token
+  const username = 'cheezecakee'; 
+  const token = process.env.REACT_APP_GITHUB_TOKEN;
 
   useEffect(() => {
     const fetchRepos = async () => {
-      const response = await fetch(`https://api.github.com/users/${username}/repos`, {
-        headers: {
-          Authorization: `token ${token}`
-        }
-      });
-      const data = await response.json();
-
-      const reposWithLanguages = await Promise.all(data.map(async repo => {
-        const langResponse = await fetch(repo.languages_url, {
+      try {
+        const response = await fetch(`https://api.github.com/users/${username}/repos`, {
           headers: {
             Authorization: `token ${token}`
           }
         });
-        const languages = await langResponse.json();
-        return { ...repo, languages };
-      }));
 
-      setRepos(reposWithLanguages);
+        const data = await response.json();
+        console.log('Data:', data); // Check fetched data
+
+        const reposWithLanguages = await Promise.all(data.map(async repo => {
+          const langResponse = await fetch(repo.languages_url, {
+            headers: {
+              Authorization: `token ${token}`
+            }
+          });
+          const languages = await langResponse.json();
+          return { ...repo, languages };
+        }));
+
+        setRepos(reposWithLanguages);
+      } catch (error) {
+        console.error('Error fetching repositories:', error);
+      }
     };
 
     fetchRepos();
@@ -44,7 +50,7 @@ const Projects = () => {
                   <span key={lang}>{lang}</span>
                 ))}
               </div>
-              <a href={repo.html_url} target="_blank" rel="noopener noreferrer" className="repo-link">[View]</a>
+              <a href={repo.html_url} target="_blank" rel="noopener noreferrer" className="repo-link">[GitHub]</a>
             </div>
           </div>
         ))}
