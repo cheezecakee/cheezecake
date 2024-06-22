eb0156eb934fe510d4709eb594a08fdb5f102714
Anabel <anabel.pmoraes@gmail.com>
2024-06-22 14:49:00 -0300

    update.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,44 +1,43 @@
 import React, { useState, useEffect } from 'react';
 
+
 const Projects = () => {
   const [repos, setRepos] = useState([]);
   const username = 'cheezecakee'; 
-  const token = process.env.github_token;
+  const token = 'REACT_APP_GITHUB_TOKEN_PLACEHOLDER';
 
-useEffect(() => {
-  const fetchRepos = async () => {
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}/repos`, {
-        headers: {
-          Authorization: `${token}`
-        }
-      });
-
-      if (!response.ok) throw new Error('Failed to fetch repositories');
-
-      const data = await response.json();
-      console.log('Data:', data);
-
-      const reposWithLanguages = await Promise.all(data.map(async repo => {
-        const langResponse = await fetch(repo.languages_url, {
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/${username}/repos`, {
           headers: {
-            Authorization: `${token}`
+            Authorization: `token ${token}`
           }
         });
-        if (!langResponse.ok) throw new Error('Failed to fetch languages');
-        const languages = await langResponse.json();
-        return {...repo, languages };
-      }));
 
-      // Filter out any undefined entries that might occur if a language URL fails
-      setRepos(reposWithLanguages.filter(Boolean));
-    } catch (error) {
-      console.error('Error fetching repositories:', error);
-    }
-  };
+        const data = await response.json();
+        console.log('Data:', data); 
+        
+        const reposWithLanguages = await Promise.all(data.map(async repo => {
+          const langResponse = await fetch(repo.languages_url, {
+            headers: {
+              Authorization: `token ${token}`
+            }
+          });
+          const languages = await langResponse.json();
+          return { ...repo, languages };
+        }));
 
-  fetchRepos();
-}, [username, token]);  return (
+        setRepos(reposWithLanguages);
+      } catch (error) {
+        console.error('Error fetching repositories:', error);
+      }
+    };
+
+    fetchRepos();
+  }, [username, token]);
+
+  return (
     <div className="projects">
       <h2>Projects</h2>
       <div className="repo-list">
@@ -62,3 +61,4 @@ useEffect(() => {
 };
 
 export default Projects;
+
